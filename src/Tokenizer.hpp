d45f76516b1402b977228c6ced97aebaa5814d77
thatrandomguy43 <126349923+thatrandomguy43@users.noreply.github.com>
2023-08-22 13:28:03 +0200

    added more tokens i forgot about

--- src/Tokenizer.hpp
+++ src/Tokenizer.hpp
@@ -9,6 +9,7 @@ enum TokenType
 {
     identifier,
 
+    literal_bool,
     literal_integer,
     literal_float,
     literal_char,
@@ -18,17 +19,22 @@ enum TokenType
     keyword_const,
     keyword_function,
     keyword_class,
+    keyword_namespace,
 
     keyword_do,
     keyword_end,
     keyword_if,
+    keyword_else,
     keyword_while,
     keyword_for,
+    keyword_in,
+    keyword_break,
 
     operator_plus,
     operator_minus,
     operator_multiply,
     operator_divide,
+    operator_modulo,
 
     operator_logical_or,
     operator_logical_and,
@@ -45,8 +51,31 @@ enum TokenType
     operator_bitwise_or,
     operator_bitwise_and,
     operator_bitwise_not,
+    operator_bitwise_xor,
     operator_shift_left,
-    operator_shift_right
+    operator_shift_right,
+
+    operator_address,
+    operator_pointer,
+    operator_reference,
+    operator_member_access,
+    operator_scope_resolution,
+    keyword_new,
+    keyword_delete,
+    keyword_this,
+
+
+    //these can't really be handled like operators, because they have stuff inside them and are somewhat ambiguous
+    open_parentheses,
+    close_parentheses,
+    open_bracket,
+    close_bracket,
+    open_brace,
+    close_brace,
+
+    comma,
+    newline,
+
 
     //ones i can think of rn, some of these will take a while to be added
 };
