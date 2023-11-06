9a04597cf2f63912d05d71788d66ebb1d72248ac
thatrandomguy43 <asger.raegaard@gmail.com>
2023-11-06 20:24:29 +0100

    reset scratchpad

--- scratchpad.cpp
+++ scratchpad.cpp
@@ -2,18 +2,9 @@
 #include <memory>
 using namespace std;
 
-int nogood()
-{
-    int a = 4;
-    cout << a;
-    if(a == 5)
-    {
-        return 1;
-    }
-}
 
 int main(int argc, char** argv)
 {
-    int x = nogood();
+
     return 0;
 }
